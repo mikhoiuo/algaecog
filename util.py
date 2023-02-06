337f411457d659770aa3ba0aba4ebba165b52a08
mikhoiuo <demibabeslay@gmail.com>
2023-02-06 13:45:15 -0500

    added util2, some sample images

--- util.py
+++ util.py
@@ -5,4 +5,5 @@ for i, filename in enumerate(os.listdir(collection)):
     os.rename("unprocessed/" + filename, "unprocessed/" + str(i) + ".png")
 
 # copied from stackoverflow bc i didnt want to deal with os
+# idk why but if u run it more than once, it deletes 1 image. so dont run it more than once.
 # https://stackoverflow.com/a/47105286
