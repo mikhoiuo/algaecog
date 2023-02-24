5966fbaeae395353b371f6be818f830c0f6d0c49
mikh <124167567+mikhoiuo@users.noreply.github.com>
2023-02-24 09:36:13 -0500

    Update train.py

--- train.py
+++ train.py
@@ -1,45 +1,18 @@
 # there is PROBABLY a better way to do this. but i am stupid.
 # import shit yk the drill
-import csv
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.preprocessing import image
 import numpy as np
+import pandas as pd
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
 
 # csv -> here
-with open('dataset.csv', 'r') as file:
-    reader = csv.reader(file)
-    # Skip the header row
-    next(reader)
-    X = []
-    y = []
-    for row in reader:
-        # resize
-        img = image.load_img(row[0], target_size=(256, 256))
-        # normalification hypno
-        x = image.img_to_array(img) / 255.0
-        X.append(x)
-        y.append(float(row[1]))
+data = pd.read_csv('data.csv')
 
-# numpification hypno
-X = np.array(X)
-y = np.array(y)
+X = data['image_path'].values
+y = data['algae_count'].values
 
-# sets
-train_ratio = 0.6
-val_ratio = 0.2
-test_ratio = 0.2
-train_size = int(train_ratio * len(X))
-val_size = int(val_ratio * len(X))
-test_size = len(X) - train_size - val_size
-
-train_X = X[:train_size]
-train_y = y[:train_size]
-val_X = X[train_size:train_size+val_size]
-val_y = y[train_size:train_size+val_size]
-test_X = X[-test_size:]
-test_y = y[-test_size:]
+# it's less hideous now im SORRY mr k i'm just on another level
 
 model = Sequential()
 
@@ -55,7 +28,7 @@ model.add(Conv2D(64, (3, 3)))
 model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Flatten())
+model.add(Flatten()) 
 model.add(Dense(64))
 model.add(Activation('relu'))
 model.add(Dropout(0.5))
@@ -63,8 +36,8 @@ model.add(Dense(1))
 model.add(Activation('sigmoid'))
 
 model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
-model.fit(train_X, train_y, batch_size=32, epochs=10, validation_data=(val_X, val_y))
 
-# eval of course i'm a classy dude
-test_loss, test_acc = model.evaluate(test_X, test_y)
-print('Test accuracy:', test_acc)
+model.fit(X, y, batch_size=32, epochs=10, validation_split=0.2)
+
+# eval because i'm not a neanderthal 
+# eval goes here because guess what i am a neanderthal
