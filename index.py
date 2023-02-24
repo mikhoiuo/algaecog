13b40c079f8c62a8e1c1c2cf24132fe18d244c09
mikh <124167567+mikhoiuo@users.noreply.github.com>
2023-02-24 09:39:22 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,27 +1,23 @@
-from predict_and_box import predict_and_box
-import cv2
-import numpy as np
 import os
 import imghdr
-from classify import predict_algae
+import cv2
+from predict_and_box import predict_and_box
 
 def process_image(image_path):
     image_type = imghdr.what(image_path)
-    if image_type not in ['png', 'jpeg']:
-        return False
-    elif image_type == 'jpeg':
-        image = cv2.imread(image_path)
+    if image_type not in {'png', 'jpeg'}:
+        return None
+
+    if image_type == 'jpeg':
+        # convert
         png_path = os.path.splitext(image_path)[0] + '.png'
+        image = cv2.imread(image_path)
         cv2.imwrite(png_path, image)
         image_path = png_path
+    else:
+        image = cv2.imread(image_path)
 
-    image = cv2.imread(image_path)
-    image = cv2.resize(image, (256, 256))
-    prediction = predict_algae(image)
-
-    # Draw bounding boxes on image
-    for box in prediction:
-        x1, y1, x2, y2 = box
-        cv2.rectangle(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    # boxes...
+    prediction = predict_and_box(model, image_path)
 
     return image, prediction, True
